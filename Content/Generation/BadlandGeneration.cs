e9c3e4cf23b705473f8720889992bf2d1d82f96a
Sulfur <vi.inparadise@gmail.com>
2026-02-03 15:53:07 -0300

    Welcome to the underground

--- Content/Generation/BadlandGeneration.cs
+++ Content/Generation/BadlandGeneration.cs
@@ -19,10 +19,10 @@ namespace Badlands.Content.Generation
     public class BadlandGeneration : ModSystem
     {
         //Generation values
-        public static int PlaceX;
-        public static int PlaceY;
-        public static int BiomeWidth;
-        public static int BiomeHeightLimit;
+        static int PlaceX;
+        static int PlaceY;
+        static int BiomeWidth;
+        static int BiomeHeightLimit;
 
         private void BadlandGen(GenerationProgress progress, GameConfiguration configuration)
         {
@@ -30,7 +30,7 @@ namespace Badlands.Content.Generation
 
             //Biome width and height
             BiomeWidth = Main.maxTilesX >= 8400 ? 289 : Main.maxTilesX >= 6400 ? 231 : 185;
-			BiomeHeightLimit = (int)Main.worldSurface;
+			BiomeHeightLimit = Main.maxTilesY / 2;
 
             //Get a good area to place
             int minX;
@@ -77,48 +77,61 @@ namespace Badlands.Content.Generation
                 }
 			}
 
-            PlaceY = (int)Main.worldSurface - (Main.maxTilesY / 8);
+            int heightLeft = FindGround(PlaceX - BiomeWidth) - 10;
+            int heightRight = FindGround(PlaceX + BiomeWidth) - 10;
+
+            if (heightLeft > (Main.worldSurface - 35) && heightRight > (Main.worldSurface - 35))
+            {
+                PlaceY = (int)Main.worldSurface - 45;
+            }
+            else
+            {
+                PlaceY = (heightLeft < heightRight) ? heightLeft : heightRight;
+            }
+
+            int StartX = PlaceX - BiomeWidth;
+			int EndX = PlaceX + BiomeWidth;
 
             //Tile replacement loop
-            for (int x = PlaceX - BiomeWidth; x <= PlaceX + BiomeWidth; x++)
+            for (int x = StartX; x <= EndX; x++)
             {
+                //Progress setters
+				progress.Set((float)(x - StartX) / (EndX - StartX));
+
                 for (int y = PlaceY; y < BiomeHeightLimit; y++)
                 {
-                    if (WorldgenTools.NoFloatingIslands(x, y, 45))
+                    Tile tile = Main.tile[x, y];
+
+					//Replace tiles
+					if (tile.HasTile && tile.TileType != TileID.Cloud && tile.TileType != TileID.RainCloud)
 					{
-						Tile tile = Main.tile[x, y];
-
-						//Replace tiles
-						if (tile.HasTile && tile.TileType != TileID.Cloud && tile.TileType != TileID.RainCloud)
-						{
-							//Clear if not solid
-							if (WorldGen.SolidTile(x, y) && (tile.TileType != TileID.LeafBlock || tile.TileType != TileID.LivingWood))
-							{
-								tile.TileType = TileID.Sandstone;
-							}
-							else
-							{
-								Main.tile[x, y].ClearEverything();
-							}
-						}
-
-                        //Place tiles on ebonstone and crimstone walls because THEY SUCK
-						if (!tile.HasTile && (tile.WallType == WallID.EbonstoneUnsafe || tile.WallType == WallID.CrimstoneUnsafe))
+						//Clear if not solid
+						if (WorldGen.SolidTile(x, y) && (tile.TileType != TileID.LeafBlock || tile.TileType != TileID.LivingWood))
 						{
 							tile.TileType = TileID.Sandstone;
 						}
-
-						//Replace walls
-						if (tile.WallType > WallID.None)
+						else
 						{
-							tile.WallType = WallID.Sandstone;
+							Main.tile[x, y].ClearEverything();
 						}
+					}
 
-						//Place walls
-						if(tile.WallType == WallID.None && y > (int)Main.worldSurface)
-                        {
-                            WorldGen.PlaceWall(x, y, WallID.Sandstone, true);
-                        }
+                    //Place tiles on ebonstone and crimstone walls because THEY SUCK
+					if (!tile.HasTile && (tile.WallType == WallID.EbonstoneUnsafe || tile.WallType == WallID.CrimstoneUnsafe))
+					{
+						tile.TileType = TileID.Sandstone;
+					}
+
+					//Replace walls
+					if (tile.WallType > WallID.None)
+					{
+						tile.WallType = WallID.Sandstone;
+					}
+
+					//Place walls
+					if(tile.WallType == WallID.None && y > (int)Main.worldSurface)
+                    {
+                        WorldGen.PlaceWall(x, y, WallID.Sandstone, true);
                     }
                 }
             }
@@ -139,64 +152,101 @@ namespace Badlands.Content.Generation
 			}
         }
 
-        public void BadlandFlattening(GenerationProgress progress, GameConfiguration configuration)
+        private void BadlandFlattening(GenerationProgress progress, GameConfiguration configuration)
         {
+            progress.Message = "This is some kind of... Badlands,,";
+
             int startX = PlaceX - BiomeWidth;
             int endX = PlaceX + BiomeWidth;
             int middleX = PlaceX;
 
             //Ground points
-            int leftY = 0;
-            int rightY = 0;
+            int leftY = FindGround(startX);
+            int rightY = FindGround(endX);
 
-            //Search values
-            bool foundGroundLeft = false;
-            int attemptsLeft = 0;
-
-            //Find values
-            while (!foundGroundLeft && attemptsLeft++ < 100000)
-			{
-				if (Main.tile[startX, leftY].TileType != TileID.Sandstone || !WorldgenTools.NoFloatingIslands(startX, leftY, 45) && leftY < Main.maxTilesY)
-				{
-					leftY++;
-				}
-
-				if ((WorldGen.SolidTile(startX, leftY) || Main.tile[startX, leftY].WallType > WallID.None) && WorldgenTools.NoFloatingIslands(startX, leftY, 45))
-				{
-					foundGroundLeft = true;
-				}
-			}
-
-            bool foundGroundRight = false;
-            attemptsLeft = 0;
-
-            while (!foundGroundRight && attemptsLeft++ < 100000)
-			{
-				if (Main.tile[endX, rightY].TileType != TileID.Sandstone || !WorldgenTools.NoFloatingIslands(endX, rightY, 45) && rightY < Main.maxTilesY)
-				{
-					rightY++;
-				}
-
-				if ((WorldGen.SolidTile(endX, rightY) || Main.tile[endX, rightY].WallType > WallID.None) && WorldgenTools.NoFloatingIslands(endX, rightY, 45))
-				{
-					foundGroundRight = true;
-				}
-			}
-
-            int middleY = leftY > rightY ? leftY : rightY;
+            int middleY = leftY < rightY ? leftY : rightY;
 
 			if (Math.Abs(leftY - rightY) > 100)
 			{
-				middleY -= (Math.Abs(leftY - rightY) / 2) + 25;
+				middleY += (Math.Abs(leftY - rightY) / 2) - 25;
 			}
 
             //Connect points
             ConnectPoints(new Vector2(startX, leftY), new Vector2(middleX, middleY), new Vector2(endX, rightY));
         }
 
+        private void BadlandCaves(GenerationProgress progress, GameConfiguration configuration)
+        {
+            progress.Message = "Caving fr fr :fire:";
+
+            int middlePoint = FindGround(PlaceX) + 25;
+
+            //Width and height
+            int undergroundWidth = BiomeWidth + 10;
+            int undergroundHeight = (int)Math.Abs(middlePoint - (Main.maxTilesY * 0.65f));
+            undergroundHeight /= 2;
+
+            //Origin in the Y axis
+            int originY = middlePoint + undergroundHeight;
+
+            //Place an oval
+            WorldUtils.Gen(new Point(PlaceX, originY), new Shapes.Circle(undergroundWidth, undergroundHeight), Actions.Chain(new GenAction[]
+            {
+                new Modifiers.Blotches(2, 0.4),
+                new Actions.SetTile(TileID.Sandstone, true, true),
+                new Actions.ClearWall(true),
+            }));
+
+            //Place walls
+            WorldUtils.Gen(new Point(PlaceX, originY), new Shapes.Circle(undergroundWidth + 1, undergroundHeight + 1), Actions.Chain(new GenAction[]
+			{
+                new Modifiers.Blotches(2, 0.4),
+				new Actions.PlaceWall(WallID.Sandstone, true),
+			}));
+
+            //Cave time
+            int seed = WorldGen.genRand.Next();
+			int octaves = 5;
+			float clearChance = 0.625f;
+
+            for (int X = PlaceX - undergroundWidth - 1; X <= PlaceX + undergroundWidth + 1; X++)
+			{
+				for (int Y = originY - undergroundHeight + 25; Y < originY + undergroundHeight + 1; Y++)
+				{
+					if (WorldgenTools.InsideEllipse(X, Y, PlaceX, originY, undergroundWidth + 2, undergroundHeight + 2))
+                    {
+                        //Perlin noise values
+                        //Higher X values for more horizontal caves
+                        float horizontalOffsetNoise = WorldgenTools.PerlinNoise2D(X / 225f, Y / 225f, octaves, unchecked(seed + 1)) * 0.01f;
+                        float cavePerlinValue = WorldgenTools.PerlinNoise2D(X / 1050f, Y / 350f, octaves, seed) + 0.5f + horizontalOffsetNoise;
+                        float cavePerlinValue2 = WorldgenTools.PerlinNoise2D(X / 1050f, Y / 350f, octaves, unchecked(seed - 1)) + 0.5f;
+                        float caveNoiseMap = (cavePerlinValue + cavePerlinValue2) * 0.5f;
+                        float caveCreationThreshold = horizontalOffsetNoise * 3.5f + 0.20f;
+
+                        //Remove tiles based on the noise
+                        if ((caveNoiseMap * caveNoiseMap > caveCreationThreshold) && (WorldGen.genRand.NextFloat() < clearChance))
+                        {
+                            WorldGen.KillTile(X, Y);
+                        }
+                    }
+				}
+			}
+
+            //Smooth the noise
+            SmoothNoise(10, PlaceX, originY, undergroundWidth, undergroundHeight);
+        }
+
         public static void ConnectPoints(Vector2 p0, Vector2 p1, Vector2 p2)
         {
             int segments = 10000;
+
+            //Noise config
+            float noiseScale = 0.055f;
+            float noiseStrength = 8f;
+            int seed = WorldGen.genRand.Next();
+
+            //Do not repeat x values if possible
+            HashSet<int> processedX = new HashSet<int>();
 
             //Place the base curve
             for(int i = 0; i < segments; i++)
@@ -204,38 +254,100 @@ namespace Badlands.Content.Generation
                 float t = i / (float)segments;
                 Vector2 position = BezierCurve.CuadraticBezier(t, p0, p1, p2);
 
+                //The x value to use
+                int valueX = (int)position.X;
+
+                //Check if it was visited already, add it to the hashSet if not
+                if (processedX.Contains(valueX)) continue;
+                processedX.Add(valueX);
+
+                //N O I S E
+                float noiseOffset = (WorldgenTools.Perlin(valueX * noiseScale, seed, 3, 0.5f) - 0.5f) * noiseStrength;
+                int valueY = (int)(position.Y + noiseOffset);
+
                 //Place tiles
-                for (int y = (int)position.Y; y < (int)Main.worldSurface; y++)
+                for (int y = valueY; y < (int)Main.worldSurface; y++)
                 {
-                    Tile tile = Main.tile[(int)position.X, y];
+                    if (y < 0 || y >= Main.maxTilesY) continue;
+
+                    Tile tile = Main.tile[valueX, y];
 
 					if (!tile.HasTile)
 					{
-						WorldGen.PlaceTile((int)position.X, y, TileID.Sandstone, true);
-                        WorldGen.PlaceWall((int)position.X, y, WallID.Sandstone, true);
+						WorldGen.PlaceTile(valueX, y, TileID.Sandstone, true);
+                        WorldGen.PlaceWall(valueX, y, WallID.Sandstone, true);
 					}
                 }
 
                 //Clear tiles above the line
 				int heightLimit = (int)(Main.worldSurface * 0.35f);
 
-				for (int y = heightLimit; y <= (int)position.Y; y++)
+				for (int y = heightLimit; y <= valueY; y++)
 				{
-					if(y != (int)position.Y)
+					if(y != valueY && WorldgenTools.NoFloatingIslands(valueX, y, 45))
                     {
-                        if (Main.tile[(int)position.X, y].TileType == TileID.Sandstone || Main.tile[(int)position.X, y].WallType == WallID.Sandstone)
-                        {
-                            Main.tile[(int)position.X, y].ClearEverything();
-                        }
+                        Main.tile[valueX, y].ClearEverything();
                     }
                     else
                     {
-                        if(Main.tile[(int)position.X, y].WallType != WallID.None)
+                        if(Main.tile[valueX, y].WallType != WallID.None)
                         {
-                            WorldGen.KillWall((int)position.X, y);
+                            WorldGen.KillWall(valueX, y);
                         }
                     }
 				}
+            }
+        }
+
+        //Find ground
+		public static int FindGround(int x)
+		{
+			int y = 0;
+
+			//Search values
+			bool foundGround = false;
+			int attemptsLeft = 0;
+
+			//Find values
+			while (!foundGround && attemptsLeft++ < 100000)
+			{
+				if (!WorldGen.SolidTile(x, y) || !WorldgenTools.NoFloatingIslands(x, y, 45) && y < Main.maxTilesY)
+				{
+					y++;
+				}
+
+				if ((WorldGen.SolidTile(x, y) || Main.tile[x, y].WallType > WallID.None) && WorldgenTools.NoFloatingIslands(x, y, 45))
+				{
+					foundGround = true;
+				}
+			}
+
+			return y;
+		}
+
+        static public void SmoothNoise(int loop, int cx, int cy, int w, int h)
+        {
+            for (int l = 0; l < loop; l++)
+            {
+                for (int x = cx - w; x <= cx + w; x++)
+                {
+                    for(int y = cy - h; y <= cy + h; y++)
+                    {
+                        if (WorldgenTools.InsideEllipse(x, y, cx, cy, w + 3, h + 3))
+                        {
+                            int tileCount = WorldgenTools.CheckTiles(x, y);
+
+                            if (tileCount > 4)
+                            {
+                                WorldGen.PlaceTile(x, y, TileID.Sandstone, true);
+                            }
+                            else if (tileCount < 4)
+                            {
+                                WorldGen.KillTile(x, y);
+                            }
+                        }
+                    }
+                }
             }
         }
 
@@ -247,6 +359,7 @@ namespace Badlands.Content.Generation
 			{
 				tasks.Insert(BiomesIndex + 1, new PassLegacy("Badland Biome", BadlandGen));
                 tasks.Insert(BiomesIndex + 2, new PassLegacy("Badland Flattening", BadlandFlattening));
+                tasks.Insert(BiomesIndex + 3, new PassLegacy("Badland Caves", BadlandCaves));
 			}
 		}
     }
--- Content/Generation/WorldgenTools.cs
+++ Content/Generation/WorldgenTools.cs
@@ -32,5 +32,180 @@ namespace Badlands.Content.Generation
 
 			return true;
 		}
+
+		static public int MooreTiles(int x, int y)
+        {
+            int count = 0;
+
+            for (int nebX = x - 1; nebX <= x + 1; nebX++)
+            {
+                for (int nebY = y - 1; nebY <= y + 1; nebY++)
+                {
+                    if (nebX != x || nebY != y)
+                    {
+                        if (Main.tile[nebX, nebY].HasTile)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+		static public int NeumanTiles(int x, int y)
+        {
+            int count = 0;
+
+            if (Main.tile[x, y - 1].HasTile) count++;
+			if (Main.tile[x - 1, y].HasTile) count++;
+			if (Main.tile[x + 1, y].HasTile) count++;
+			if (Main.tile[x, y + 1].HasTile) count++;
+
+            return count;
+        }
+
+		static public int CheckTiles(int x, int y)
+        {
+            int count = 0;
+
+            for (int nebX = x - 1; nebX <= x + 1; nebX++)
+            {
+                for (int nebY = y - 1; nebY <= y + 1; nebY++)
+                {
+                    if (nebX != x || nebY != y)
+                    {
+                        if (Main.tile[nebX, nebY].HasTile)
+                        {
+                            count++;
+                        }
+                    }
+                }
+            }
+
+            return count;
+        }
+
+		static public bool InsideEllipse(int x, int y, int h, int k, int a, int b)
+        {
+            double powXH = Math.Pow((x-h), 2);
+            double powA = Math.Pow(a, 2);
+            double powYK = Math.Pow((y-k), 2);
+            double powB = Math.Pow(b, 2);
+
+            double sum1 = powXH / powA;
+            double sum2 = powYK / powB;
+
+            double total = sum1 + sum2;
+
+            return total <= 1;
+        }
+
+		#region "Perlin noise simple"
+		private static float Fade(float t) => t * t * t * (t * (t * 6 - 15) + 10);
+
+		private static float Lerp(float a, float b, float t) => a + t * (b - a);
+
+		private static float Grad(int p) => (p & 1) == 0 ? 1f : -1f;
+
+		private static uint StableHash(int x)
+		{
+			uint u = (uint)x;
+			u = ((u >> 16) ^ u) * 0x45d9f3b;
+			u = ((u >> 16) ^ u) * 0x45d9f3b;
+			u = (u >> 16) ^ u;
+			return u;
+		}
+
+		private static float Perlin1D(float x, int seed)
+		{
+			int xi = (int)MathF.Floor(x);
+			float xf = x - xi;
+
+			int h0 = (int)StableHash(xi + seed);
+			int h1 = (int)StableHash(xi + 1 + seed);
+
+			float d0 = Grad(h0) * xf;
+			float d1 = Grad(h1) * (xf - 1);
+
+			float u = Fade(xf);
+
+			return Lerp(d0, d1, u);
+		}
+
+		public static float Perlin(float x, int seed, int octaves = 4, float persistence = 0.5f)
+		{
+			float total = 0f;
+			float frequency = 1f;
+			float amplitude = 1f;
+			float maxValue = 0f;
+
+			for (int i = 0; i < octaves; i++)
+			{
+				total += Perlin1D(x * frequency, seed + i) * amplitude;
+				maxValue += amplitude;
+
+				amplitude *= persistence;
+				frequency *= 2f;
+			}
+
+			return (total / maxValue) * 0.5f + 0.5f;
+		}
+		#endregion
+
+		#region "Perlin noise 2D"
+		internal static readonly List<Vector2> Directions = new List<Vector2>()
+		{
+			new Vector2(-1f, -1f),
+			new Vector2(1f, -1f),
+			new Vector2(-1f, 1f),
+			new Vector2(1f, 1f),
+			new Vector2(0f, -1f),
+			new Vector2(-1f, 0f),
+			new Vector2(0f, 1f),
+			new Vector2(1f, 0f),
+		};
+		
+		public static float PerlinNoise2D(float x, float y, int octaves, int seed)
+		{
+			float SmoothFunction(float n) => 3f * n * n - 2f * n * n * n;
+
+			float NoiseGradient(int s, int noiseX, int noiseY, float xd, float yd)
+			{
+				int hash = s;
+				hash ^= 1619 * noiseX;
+				hash ^= 31337 * noiseY;
+
+				hash = hash * hash * hash * 60493;
+				hash = (hash >> 13) ^ hash;
+
+				Vector2 g = Directions[hash & 7];
+
+				return xd * g.X + yd * g.Y;
+			}
+
+			int frequency = (int)Math.Pow(2D, octaves);
+			x *= frequency;
+			y *= frequency;
+
+			int flooredX = (int)x;
+			int flooredY = (int)y;
+			int ceilingX = flooredX + 1;
+			int ceilingY = flooredY + 1;
+			float interpolatedX = x - flooredX;
+			float interpolatedY = y - flooredY;
+			float interpolatedX2 = interpolatedX - 1;
+			float interpolatedY2 = interpolatedY - 1;
+
+			float fadeX = SmoothFunction(interpolatedX);
+			float fadeY = SmoothFunction(interpolatedY);
+
+			float smoothX = MathHelper.Lerp(NoiseGradient(seed, flooredX, flooredY, interpolatedX, interpolatedY), NoiseGradient(seed, ceilingX, flooredY, interpolatedX2, interpolatedY), fadeX);
+			float smoothY = MathHelper.Lerp(NoiseGradient(seed, flooredX, ceilingY, interpolatedX, interpolatedY2), NoiseGradient(seed, ceilingX, ceilingY, interpolatedX2, interpolatedY2), fadeX);
+
+			return MathHelper.Lerp(smoothX, smoothY, fadeY);
+		}
+		#endregion
 	}
 }
